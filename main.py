154e91518b016d9bd0f1310aa8cb3f006dfae1e3
ayushmaheshwari-cyn <ayushmaheshwari@cyntexa.com>
2026-08-05 18:11:41 +0530

    second commit

--- main.py
+++ main.py
@@ -226,53 +226,53 @@
 - There are two modes for field_validator, before and after. By default mode is after. The before mode is used to validate the data before the type_validation and after mode is used to validate the data after the type_validation.
 """
 
-from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
-from typing import List, Dict, Optional, Annotated
+# from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
+# from typing import List, Dict, Optional, Annotated
 
-class Patient(BaseModel): 
-    name: str
-    email: EmailStr
-    age: int
-    weight: float
-    married: bool
-    allergies: List[str]
-    contact_details: Dict[str, str]
+# class Patient(BaseModel): 
+#     name: str
+#     email: EmailStr
+#     age: int
+#     weight: float
+#     married: bool
+#     allergies: List[str]
+#     contact_details: Dict[str, str]
 
-    @field_validator('email')    # validate email for specific domains
-    @classmethod
-    def email_validator(cls, value):
-         valid_domains = ['hdfc.com', 'icici.com']
-         domain_name = value.split('@')[-1]
-         if domain_name not in valid_domains:
-             raise ValueError(f"Invalid email domain. Allowed domains are: {valid_domains}")
-         return value 
+#     @field_validator('email')    # validate email for specific domains
+#     @classmethod
+#     def email_validator(cls, value):
+#          valid_domains = ['hdfc.com', 'icici.com']
+#          domain_name = value.split('@')[-1]
+#          if domain_name not in valid_domains:
+#              raise ValueError(f"Invalid email domain. Allowed domains are: {valid_domains}")
+#          return value 
 
-    @field_validator('name')      # transform name to uppercase
-    @classmethod
-    def name_validator(cls, value): 
-        return value.upper() 
+#     @field_validator('name')      # transform name to uppercase
+#     @classmethod
+#     def name_validator(cls, value): 
+#         return value.upper() 
 
-    @field_validator('age', mode='after')      # default mode is after 
-    @classmethod
-    def age_validator(cls, value): 
-        if 0<value< 100:
-            return value
-        raise ValueError("Age must be between 0 and 100")
+#     @field_validator('age', mode='after')      # default mode is after 
+#     @classmethod
+#     def age_validator(cls, value): 
+#         if 0<value< 100:
+#             return value
+#         raise ValueError("Age must be between 0 and 100")
 
        
     
-def update_patient_data(patient: Patient): 
-    print(patient.name)
-    print(patient.email)
-    print(patient.age)
-    print(patient.married)
-    print(patient.allergies)
-    print("Updated Successfully ✅")
+# def update_patient_data(patient: Patient): 
+#     print(patient.name)
+#     print(patient.email)
+#     print(patient.age)
+#     print(patient.married)
+#     print(patient.allergies)
+#     print("Updated Successfully ✅")
 
-patient_info = {'name': 'John Doe', 'email': 'abc@hdfc.com', 'age': '30', 'weight': 70.5, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details': {'phone': '123-456-7890'}}
-patient1 = Patient(**patient_info)
+# patient_info = {'name': 'John Doe', 'email': 'abc@hdfc.com', 'age': '30', 'weight': 70.5, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details': {'phone': '123-456-7890'}}
+# patient1 = Patient(**patient_info)
 
-update_patient_data(patient1)
+# update_patient_data(patient1)
 
 
 
